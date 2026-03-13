04749e8f1642378d107f775db5cd1dc6713a4e8a
RAMEES K <161185637+rk897@users.noreply.github.com>
2026-03-13 15:24:43 +0530

    Add database connection and visitor tracking
    Establish database connection and track visitor count.

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
-echo "<h1><h1>Welcome Ramees - done</h1></h1>";
-echo "<p>Server Time: " . date("Y-m-d H:i:s") . "</p>";
+$conn = new mysqli("project-db.clymw264oc2j.ap-south-1.rds.amazonaws.com","admin","Project456","webapp");
+
+if ($conn->connect_error) {
+    die("Connection failed");
+}
+
+$conn->query("INSERT INTO visitors() VALUES()");
+
+$result = $conn->query("SELECT COUNT(*) as total FROM visitors");
+$row = $result->fetch_assoc();
+
+echo "<h1>Welcome Ramees</h1>";
+echo "<p>Total Visits: ".$row['total']."</p>";
 ?>
